b51dd79eee4acd45d4cbf48bdc58e1a031c0b12f
Brian Wong <brwwong@cs.stonybrook.edu>
2018-03-28 21:17:37 -0700

    fib math works, do the file stuff

--- part2.c
+++ part2.c
@@ -3,10 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <inttypes.h>
 
 double function(double c, int n, double array[]){
-	printf("\nCalculating f(c)\n");
-	printf("c is %lf n is %d\n",c,n);
 	double sum=0;
 	int index=0;	
 	double temp;
@@ -17,26 +16,22 @@ double function(double c, int n, double array[]){
 		index++;
 		n--;	
 	}	
-	printf("f(c)=%lf\n", sum);
 	return  sum;
 }
-int main(/*FILE* input*/){
+void app1(/*FILE* input*/){
 	FILE *input;
 	input=fopen("input.txt","r");
 	printf("app1 start \n");
 	double e,a,b,c;
 	int n;
 	fscanf(input,"%d %lf %lf %lf",&n,&e,&a,&b);
-	printf("n= %d, e= %lf, A= %lf, B= %lf\n",n,e,a,b);
+//	printf("n= %d, e= %lf, A= %lf, B= %lf\n",n,e,a,b);
 	double array[n+1];
 	int i=0;
 	while(i<n+1){
 		fscanf(input,"%lf",&array[i]);
-		printf("array[%d]=%lf ",i,array[i]);
+//		printf("array[%d]=%lf ",i,array[i]);
 		i++;
-	}
-	if(a>=b){
-		return -1;
 	}
 	//psuedocode!
 	while(1){
@@ -48,27 +43,46 @@ int main(/*FILE* input*/){
 		//if sign (f(c))=sign(f(a)) then  a<-c else b<- c
                 double fOfC=function(c,n,array);
 		double fOfA=function(a,n,array);
-		if( fOfC>0 && fOfA>0){
+		if( fOfC*fOfA>0){
 			a=c;
 		}
 		else
 			b=c;
 	}
 	printf("\nc=%f\n",c);
-	//FILE *solver=fopen("part2_solver.dat","w");
-	//output c to file solver
-	return 0;
+	FILE *solver=fopen("part2_solver.dat","w+");
+	fprintf(solver,"%lf",c);
+	fclose(solver);
 }
-void app2(){
+void app2(uint64_t n){
+
+	uint64_t array[n+1];
+	if(n==0)
+		printf("0\n");
+	if(n==1)
+		printf("1\n");
+		
+	array[0]=0;
+	array[1]=1;
+
+	uint64_t i=2;
+	while(i<=n){
+		array[i]=array[i-1]+array[i-2];
+		i++;
+	}
+	printf("array[n]= %lu\n", array[n]);
+	
+
+}
+//fibonacci
+void app3(){
 	
 }
 
-void app3(){
 
-}
-
-/*
 int main(){
+	app2(93);
+/*
 	char* openFile;
 	FILE* fileName;
 	printf("Input Filename\n");
@@ -83,6 +97,6 @@ int main(){
 	if(strcmp(operation,"solver")==0){
 		app1(fileName);
 	}
-
+*/
 	return 0;
-}*/
+}
