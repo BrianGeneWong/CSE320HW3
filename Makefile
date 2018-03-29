12c2a651da7b5424c2885497ad23ec153f5f57e5
Brian Wong <brwwong@cs.stonybrook.edu>
2018-03-28 19:58:36 -0700

    app1 almost works

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 part2:
-	gcc -g part2.c -o part2
+	gcc -g part2.c -o part2 -lm
 clean:
 	rm -f part2
--- part2.c
+++ part2.c
@@ -4,21 +4,39 @@
 #include <stdlib.h>
 #include <math.h>
 
-float function(int n, float array){
-	int i=n;
-	double sum=0;	
-	while(i>=0){
-		i++;	
+double function(double c, int n, double array[]){
+	printf("\nCalculating f(c)\n");
+	printf("c is %lf n is %d\n",c,n);
+	double sum=0;
+	int index=0;	
+	double temp;
+	while(n>=0){
+		temp=pow(c,n);
+		temp=temp*(array[index]);
+		sum+=temp;
+		index++;
+		n--;	
 	}	
-	return  0;
+	printf("f(c)=%lf\n", sum);
+	return  sum;
 }
-int app1(FILE* input){
-	float e,a,b,c;
+int main(/*FILE* input*/){
+	FILE *input;
+	input=fopen("input.txt","r");
+	printf("app1 start \n");
+	double e,a,b,c;
 	int n;
-	fscanf(input,"%d %f %f %f",&n,&e,&a,&b);
-	float array[n+1];
+	fscanf(input,"%d %lf %lf %lf",&n,&e,&a,&b);
+	printf("n= %d, e= %lf, A= %lf, B= %lf\n",n,e,a,b);
+	double array[n+1];
+	int i=0;
+	while(i<n+1){
+		fscanf(input,"%lf",&array[i]);
+		printf("array[%d]=%lf ",i,array[i]);
+		i++;
+	}
 	if(a>=b){
-
+		return -1;
 	}
 	//psuedocode!
 	while(1){
@@ -27,22 +45,29 @@ int app1(FILE* input){
 			break;
 		if((b-a)/2<e)
 			break;
-		
+		//if sign (f(c))=sign(f(a)) then  a<-c else b<- c
+                double fOfC=function(c,n,array);
+		double fOfA=function(a,n,array);
+		if( fOfC>0 && fOfA>0){
+			a=c;
+		}
+		else
+			b=c;
 	}
-	printf("%f\n",c);
-	FILE *solver=fopen("part2_solver.dat","w");
+	printf("\nc=%f\n",c);
+	//FILE *solver=fopen("part2_solver.dat","w");
 	//output c to file solver
 	return 0;
 }
-int app2(){
+void app2(){
 	
 }
 
-int app3(){
+void app3(){
 
 }
 
-
+/*
 int main(){
 	char* openFile;
 	FILE* fileName;
@@ -60,4 +85,4 @@ int main(){
 	}
 
 	return 0;
-}
+}*/
