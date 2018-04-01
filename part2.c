0ce3c73ea2fcc79f51f5c366b00aa93ce82accf5
Brian Wong <brwwong@cs.stonybrook.edu>
2018-04-01 14:42:23 -0700

    app2 now works for one test case

--- part2.c
+++ part2.c
@@ -54,8 +54,60 @@ void app1(/*FILE* input*/){
 	fprintf(solver,"%lf",c);
 	fclose(solver);
 }
-void app2(uint64_t n){
+void app2(){
+	FILE* input;
+	input=fopen("matrix.txt","r");
+	int x,y;
+	fscanf(input,"%d %d",&x,&y);
+	int matrix1[x][y];
+	//put the numbers in the matrix;
+	int i,j;
+	for (i=0;i<x;i++){
+		for(j=0;j<y;j++){
+			fscanf(input,"%d",&matrix1[i][j]);
+		//	printf("matrix at [%d][%d] is %d \n",i,j,matrix1[i][j]);
+		}
+	}
+	int a,b;
+	fscanf(input,"%d %d",&a,&b);
+	printf("a is %d and b is %d\n",a,b);
+	int matrix2[a][b];
+	for (i=0;i<a;i++){
+		for(j=0;j<b;j++){
+			fscanf(input,"%d",&matrix2[i][j]);
+	//		printf("matrix at [%d][%d] is %d \n",i,j,matrix2[i][j]);
+		}
+	}
 
+	//now we do matrix multiplication
+	//WE WILL get a square product matrix
+	int product[x][b];
+	int sum;
+	int temp;
+	int c=0;
+	for (i=0;i<x;i++){
+		for( j=0;j<b;j++){
+			sum=0;
+			for(c=0;c<y;c++){
+				temp=matrix1[i][c]*matrix2[c][j];
+				sum+=temp;
+			}
+		//	printf("sum for [%d][%d] is %d \n",i,j,sum);	
+				product[i][j]=sum;
+		}
+	}	
+
+	int trace=0;
+	for(i=0;i<x;i++){
+		trace+=product[i][i];
+	}	
+	printf("trace is %d\n",trace);
+
+}
+void app3(){
+	FILE* input;
+	int n=93;
+//	fscanf(input,"%d",n);
 	uint64_t array[n+1];
 	if(n==0)
 		printf("0\n");
@@ -65,23 +117,19 @@ void app2(uint64_t n){
 	array[0]=0;
 	array[1]=1;
 
-	uint64_t i=2;
+	int i=2;
 	while(i<=n){
 		array[i]=array[i-1]+array[i-2];
 		i++;
 	}
 	printf("array[n]= %lu\n", array[n]);
-	
+		
 
-}
-//fibonacci
-void app3(){
-	
 }
 
 
 int main(){
-	app2(93);
+	app2();
 /*
 	char* openFile;
 	FILE* fileName;
