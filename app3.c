39f48c236645452c9528f50762d68336cdc48e5c
Brian Wong <brwwong@cs.stonybrook.edu>
2018-04-03 16:30:14 -0700

    fixed fib print statments

--- app3.c
+++ app3.c
@@ -8,26 +8,30 @@
 int main (int argc,char** argv){
 	FILE* output;
 	int n=atoi(*(argv+1));
+	uint64_t final;
 //	printf("n is %d\n",n);
 //	fscanf(input,"%d",n);
 	uint64_t array[n+1];
 	if(n==0)
-		printf("0\n");
-	if(n==1)
-		printf("1\n");
+		final=0;
+	else if(n==1)
+		final=1;
 		
-	array[0]=0;
-	array[1]=1;
+	else{
+		array[0]=0;
+		array[1]=1;
 
-	int i=2;
-	while(i<=n){
-		array[i]=array[i-1]+array[i-2];
-		i++;
+		int i=2;
+		while(i<=n){
+			array[i]=array[i-1]+array[i-2];
+			i++;
+		}
+		final=array[n];
 	}
 //	printf("array[n]= %lu\n", array[n]);
 	char* outFile="part2_fib.dat";
 	output=fopen(outFile,"w+");
-	fprintf(output,"%lu",array[n]);
+	fprintf(output,"%lu",final);
 	fclose(output);	
 	return 0;
 }
